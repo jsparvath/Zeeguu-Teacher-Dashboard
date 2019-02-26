7d257acdd762078b06d07d8a21f67ff53d265cdd
mads <mkrn@itu.dk>
2019-02-26 10:17:36 +0100

    initial setup and add reach router

---
+++ zeeguu-teacher-dashboard/src/pages/Classroom.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Classroom = ({ classId }) => {
+  return <div>I am the classroom with id {classId}</div>
+}
+
+export default Classroom
---
+++ zeeguu-teacher-dashboard/src/pages/StudentActivity.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const StudentActivity = () => {
+  return <div>I am a student activity page</div>
+}
+
+export default StudentActivity
