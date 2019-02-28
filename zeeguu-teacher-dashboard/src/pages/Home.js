4aef356c59d763ab66880b49b34bd56c97fe2812
Jonathan Sparvath <jonathan.sparvath@gmail.com>
2019-02-28 22:09:15 +0100

    fixed some styling

--- zeeguu-teacher-dashboard/src/pages/Home.js
+++ zeeguu-teacher-dashboard/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getCohortsInfo } from '../api/api_endpoints'
 import CreateClass from '../components/CreateClass'
 import Button from '../components/ui/Button'
-import { MdPeople } from 'react-icons/md/'
+import { MdPeople, MdArrowForward } from 'react-icons/md/'
 
 import './Home.scss'
 
@@ -12,18 +12,16 @@ const CohortItem = ({ cohort }) => (
       <p>{cohort.language_name}</p>
       <p className="special">
         {cohort.cur_students}/{cohort.max_students}{' '}
-        <MdPeople className="cohort-card-people-icon" size="26px" />
+        <MdPeople className="cohort-card-icon-people" size="26px" />
       </p>
     </div>
     <h2 className="cohort-card-headline">{cohort.name}</h2>
     <div>
       <p className="font-light">invite code: {cohort.inv_code}</p>
       {/* <a href="#">View class </a> */}
-      <Button
-        rounded
-        onClick={() => alert('clicked')}
-      >
-        View class
+      <Button rounded onClick={() => alert('clicked')}>
+        <span className="lol">View class </span>
+        <MdArrowForward className="cohort-card-icon-btn" size="28px" />
       </Button>
     </div>
   </div>
@@ -31,9 +29,11 @@ const CohortItem = ({ cohort }) => (
 
 const HomeTemplate = ({ cohorts }) => (
   <div className="page-home">
-    {cohorts.map(cohort => (
-      <CohortItem key={cohort.id} cohort={cohort} />
-    ))}
+    <div className="page-home-content">
+      {cohorts.map(cohort => (
+        <CohortItem key={cohort.id} cohort={cohort} />
+      ))}
+    </div>
   </div>
 )
 
