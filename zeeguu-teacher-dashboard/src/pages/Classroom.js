eb7a0425c652f268563614724dc07d5d44c8787a
Mads <mkrn@itu.dk>
2019-03-03 20:22:49 +0100

    use context to keep a global "active class"

--- zeeguu-teacher-dashboard/src/pages/Classroom.js
+++ zeeguu-teacher-dashboard/src/pages/Classroom.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ClassContext from '../ClassContext'
+import AddEditClassButton from '../components/AddEditClassButton'
+import { EditClass } from '../components/EditClass'
 
-const ClassroomTemplate = ({ cohortName, cohortCode }) => (
-  <div>
-    Class Name: {cohortName} Class code: {cohortCode}
-  </div>
-)
-const Classroom = ({ classId }) => {
+// const ClassroomTemplate = ({ cohortName, cohortCode }) => (
+//   <div>
+//     Class Name: {cohortName} Class code: {cohortCode}
+//   </div>
+// )
+const Classroom = () => {
+  const test = useContext(ClassContext)
+  const cohort = test.activeClass
+  console.log('cohort', cohort)
   return (
     <div>
-      I am the classroom with id {classId}
-      <ClassroomTemplate cohortName="Spanish 2019" cohortCode="josn" />
+      I am the classroom with id {cohort.id} and name: {cohort.name}
+      <AddEditClassButton text="Edit class">
+        {closemodal => <EditClass closemodal={closemodal} />}
+      </AddEditClassButton>
     </div>
   )
 }
 
 export default Classroom
-
