9242a888697b63db64ed62cec5a2b364300537d4
mads <mkrn@itu.dk>
2019-03-05 14:08:02 +0100

    implement nav bar component and remove unused context

--- zeeguu-teacher-dashboard/src/App.js
+++ zeeguu-teacher-dashboard/src/App.js
@@ -1,39 +1,27 @@
 import React, { useState } from 'react'
-import { Router, Link } from '@reach/router'
+import { Router } from '@reach/router'
 
 import Home from './pages/Home'
 // import LoginPage from './pages/LoginPage'
 import Classroom from './pages/Classroom'
 import StudentActivity from './pages/StudentActivity'
 import ClassContext from './ClassContext'
+import Nav from './components/Nav'
 
 // import logo from './logo.svg'
 import './App.scss'
 
-const Nav = () => (
-  <div>
-    <Link to="/">Go to Home</Link>
-    <Link to="/login">Login</Link>
-    {/* <Link to="/classroom/70">Go to classroom page</Link> */}
-    <Link to="/studentactivity">Go to studentactivity page</Link>
-  </div>
-)
-
 const App = () => {
-  const [activeClass, setActiveClass] = useState(null)
   return (
-    <ClassContext.Provider value={{ activeClass, setActiveClass }}>
-      <div className="App">
-        {activeClass && <p>Current active class name: {activeClass.name}</p>}
-        <Nav />
-        <Router>
-          <Home path="/" />
-          {/* <LoginPage path="/login" /> */}
-          <Classroom path="classroom/:classId" />
-          <StudentActivity path="studentactivity" />
-        </Router>
-      </div>
-    </ClassContext.Provider>
+    <div className="App">
+      <Nav />
+      <Router>
+        <Home path="/" />
+        {/* <LoginPage path="/login" /> */}
+        <Classroom path="classroom/:classId" />
+        <StudentActivity path="studentactivity" />
+      </Router>
+    </div>
   )
 }
 
