124cd592f34b009a2b331d2bade6598ae48f63b4
Jonathan Sparvath <jonathan.sparvath@gmail.com>
2019-03-08 03:22:12 +0100

    student page pulling data from db

--- zeeguu-teacher-dashboard/src/api/apiUser.js
+++ zeeguu-teacher-dashboard/src/api/apiUser.js
@@ -1,8 +1,32 @@
 import { apiGet } from './apiEndpoints'
+/**
+ * Loads an invidiual users data.
+ * Requires permission (the logged in teacher must be a teacher of the class containing user with user_id ).
+ * @param {integer} userId used to find user.
+ * @param {integer} duration
+ * @returns {object} object containing (id, name, email, reading time, exercises done, last article)
+ */
+export function loadUserInfo(userId, duration) {
+  const studentInfo = apiGet(`/user_info/${userId}/${duration}`)
+  return studentInfo
+}
+
+export function loadUserReadingSessions(userId, duration) {
+  const userReadingSessions = apiGet(
+    `/user_reading_sessions/${userId}/${duration}`
+  ).then(({ data }) => {
+    console.log('data')
+    console.log(data)
+  })
+}
 
 export function loadUserData(userId, duration) {
   let stats = apiGet(`/cohort_member_bookmarks/${userId}/${duration}`).then(
-    ({ data }) => filterUserBookmarks(data)
+    ({ data }) => {
+      const filteredData = filterUserBookmarks(data)
+      const sortedData = sortUserBookmarks(data)
+      return sortedData
+    }
   )
   return stats
 }
@@ -12,7 +36,6 @@ export function loadUserData(userId, duration) {
  * @param {Array} data
  */
 function filterUserBookmarks(data) {
-  console.log(data)
   let wordString = ' '
 
   const result = data.map(day =>
@@ -27,4 +50,49 @@ function filterUserBookmarks(data) {
     }, [])
   )
   return result
+}
+
+function sortUserBookmarks(data) {
+  let masterList = []
+  data.map(day => {
+    let masterElement = {}
+    masterElement.date = day.date
+    masterElement['article_list'] = []
+    masterList.push(masterElement)
+    day.bookmarks.forEach(bookmark => {
+      let existsArticle = false
+      masterElement['article_list'].forEach(article => {
+        if (article.title === bookmark.title) {
+          existsArticle = true
+          let existsSentence = false
+          article.sentence_list.forEach(sentence => {
+            if (sentence.context === bookmark.context) {
+              existsSentence = true
+              sentence.bookmarks.push(bookmark)
+            }
+          })
+          if (!existsSentence) {
+            let sentenceElement = {}
+            sentenceElement.context = bookmark.context
+            sentenceElement.bookmarks = []
+            sentenceElement.bookmarks.push(bookmark)
+            article.sentence_list.push(sentenceElement)
+          }
+        }
+      })
+      if (!existsArticle) {
+        let articleElement = {}
+        articleElement.title = bookmark.title
+        articleElement.url = bookmark.url
+        articleElement.sentence_list = []
+        masterElement.article_list.push(articleElement)
+        let sentenceElement = []
+        sentenceElement.context = bookmark.context
+        sentenceElement.bookmarks = []
+        articleElement.sentence_list.push(sentenceElement)
+        sentenceElement.bookmarks.push(bookmark)
+      }
+    })
+  })
+  return masterList
 }
