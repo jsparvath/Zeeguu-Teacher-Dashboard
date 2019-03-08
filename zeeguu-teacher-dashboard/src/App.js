2aecbd4719e99b5e08fc0aa399a103594b5eb971
mads <mkrn@itu.dk>
2019-03-08 14:59:54 +0100

    begin work on timeperiod component

--- zeeguu-teacher-dashboard/src/App.js
+++ zeeguu-teacher-dashboard/src/App.js
@@ -1,22 +1,44 @@
 import { Router } from '@reach/router'
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './App.scss'
 import Nav from './components/Nav'
 import Classroom from './pages/Classroom'
 import Home from './pages/Home'
 import StudentPage from './pages/StudentPage'
+const TimePeriodContext = React.createContext({
+  timePeriod: 14,
+  setTimePeriod: () => {}
+})
 
 const App = () => {
+  const [timePeriod, setTimePeriod] = useState(14)
   return (
-    <div className="App">
-      <Nav />
-      <Router>
-        <Home path="/" />
-        {/* <LoginPage path="/login" /> */}
-        <Classroom path="classroom/:classId" />
-        <StudentPage path="student/:studentId" />
-      </Router>
+    <TimePeriodContext.Provider value={{ timePeriod, setTimePeriod }}>
+      <div className="App">
+        <Nav />
+        <TimePeriodComponent />
+        <Router>
+          <Home path="/" />
+          {/* <LoginPage path="/login" /> */}
+          <Classroom path="classroom/:classId" />
+          <StudentPage path="student/:studentId" />
+        </Router>
+      </div>
+    </TimePeriodContext.Provider>
+  )
+}
+
+const TimePeriodComponent = () => {
+  const { timePeriod, setTimePeriod } = useContext(TimePeriodContext)
+  return (
+    // <TimePeriodContext.Consumer>
+    <div>
+      <button onClick={() => setTimePeriod(30)}>
+        Switch Language (Current: {timePeriod})
+      </button>
     </div>
+
+    // </TimePeriodContext.Consumer>
   )
 }
 
