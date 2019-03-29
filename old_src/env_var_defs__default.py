d31147a3ef3cce1ac3a4d253f1aa724c88acb8d8
mads <mkrn@itu.dk>
2019-03-29 13:07:10 +0100

    remove unused files

--- old_src/env_var_defs__default.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ['TEACHER_DASHBOARD_CONFIG']=os.path.expanduser('~/<blabla>/teacher_dashboard.cfg')
--- old_src/zeeguu_teacher_dashboard/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# These imports have be issued after the app is initialized due to how Flask works.
-from .page_routes import homepage, errorpages, login, studentpage, classroom
